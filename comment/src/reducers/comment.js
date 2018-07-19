1e1a697e35a5a86bd53871372546f764ad627576
Lbh <544289495@qq.com>
2018-07-19 14:45:29 +0800

    安装redux react-redux |comment.js (redux)

---
+++ comment/src/reducers/comment.js
@@ -0,0 +1,54 @@
+// action types
+const INIT_COMMENTS = 'INIT_COMMENTS'
+const ADD_COMMENT = 'ADD_COMMENT'
+const DELETE_COMMENT = 'DELETE_COMMENT'
+
+export default function (state, action) {
+    if (!state) {
+        state = {
+            comments: []
+        }
+    }
+    switch (action.type) {
+        case INIT_COMMENTS:
+            //初始化评论
+            return {
+                comments: action.comments
+            }
+        case ADD_COMMENT:
+            //新增评论
+            return {
+                comments: [...state.comments, action.comments]
+            }
+        case DELETE_COMMENT:
+            //删除评论
+            return {
+                comments: [
+                    ...state.comments.slice(0, action.commentIndex),
+                    ...state.comments.slice(action.commentIndex + 1)
+                ]
+            }
+        default:
+            return state
+    }
+}
+
+//action creators
+export const initComments = (comments) => {
+    return {
+        type: INIT_COMMENTS,
+        comments
+    }
+}
+export const addComments = (comments) => {
+    return {
+        type: ADD_COMMENT,
+        comments
+    }
+}
+export const deleteComments = (comments) => {
+    return {
+        type: DELETE_COMMENT,
+        comments
+    }
+}
